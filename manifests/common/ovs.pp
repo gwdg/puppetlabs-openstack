dbbe3562b7df0327187bd67c694c2b13e89ee535
Eugene Nelen <eugene@nelen.org.ua>
2014-06-23 22:56:10 +0200

    Move to ML2 / OVS

--- manifests/common/ovs.pp
+++ manifests/common/ovs.pp
@@ -1,25 +1,22 @@
 #
 class openstack::common::ovs {
   $data_network = hiera('openstack::network::data')
-  $data_address = ip_for_network($data_network)
-  $enable_tunneling = hiera('openstack::neutron::tunneling')
-  $tunnel_types = hiera('openstack::neutron::tunnel_types')
-  $tenant_network_type = hiera('openstack::neutron::tenant_network_type')
-  $network_vlan_ranges = hiera('openstack::neutron::network_vlan_ranges')
+  $data_address = ip_for_network(hiera('openstack::network::data'))
 
-  class { '::neutron::agents::ovs':
+  class { '::neutron::agents::ml2::ovs':
+    enable_tunneling        => hiera('openstack::neutron::tunneling'),
+    local_ip                => ip_for_network(hiera('openstack::network::data')),
+    enabled                 => true,
+    tunnel_types            => hiera('openstack::neutron::tunnel_types'),
 
-    enable_tunneling => true,
-    local_ip         => $data_address,
-    enabled          => true,
-    tunnel_types     => ['gre',],
-
-#    firewall_driver  => 'quantum.agent.linux.iptables_firewall.OVSHybridIptablesFirewallDriver',
-#    polling_interval => 2,
+#    firewall_driver        => 'quantum.agent.linux.iptables_firewall.OVSHybridIptablesFirewallDriver',
+#    polling_interval       => 2,
   }
 
-  class  { '::neutron::plugins::ovs':
-    tenant_network_type => $tenant_network_type,
-    network_vlan_ranges => $network_vlan_ranges
+  class  { '::neutron::plugins::ml2':
+    type_drivers            => hiera('openstack::neutron::type_drivers'),
+    tenant_network_types    => hiera('openstack::neutron::tenant_network_type'),
+    mechanism_drivers       => hiera('openstack::neutron::mechanism_drivers'),
+    tunnel_id_ranges        => hiera('openstack::neutron::tunnel_id_ranges'),
   }
 }
