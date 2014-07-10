ffdf90c6681e71e201169d914c881f058f5c58a9
Piotr Kasprzak <piotr.kasprzak@gwdg.de>
2014-07-10 13:14:21 +0200

    Removed default parameters

--- manifests/common/ovs.pp
+++ manifests/common/ovs.pp
@@ -1,13 +1,15 @@
+#
 class openstack::common::ovs {
   $data_network = hiera('openstack::network::data')
   $data_address = ip_for_network($data_network)
-  $enable_tunneling = hiera('openstack::neutron::tunneling', true)
-  $tunnel_types = hiera('openstack::neutron::tunnel_types', ['gre'])
-  $tenant_network_type = hiera('openstack::neutron::tenant_network_type', 'gre')
-  $network_vlan_ranges = hiera('openstack::neutron::network_vlan_ranges', 'physnet1:1000:2000')
+  $enable_tunneling = hiera('openstack::neutron::tunneling')
+  $tunnel_types = hiera('openstack::neutron::tunnel_types')
+  $tenant_network_type = hiera('openstack::neutron::tenant_network_type')
+  $network_vlan_ranges = hiera('openstack::neutron::network_vlan_ranges')
 
   class { '::neutron::agents::ovs':
-    enable_tunneling => 'True',
+
+    enable_tunneling => true,
     local_ip         => $data_address,
     enabled          => true,
     tunnel_types     => ['gre',],
