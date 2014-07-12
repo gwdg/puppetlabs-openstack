13645466d1c1d120012deccab18c5c3e06ab6574
Piotr Kasprzak <piotr.kasprzak@gwdg.de>
2014-07-12 15:29:01 +0200

    Disable s3 API for now in firewall

--- manifests/profile/nova/api.pp
+++ manifests/profile/nova/api.pp
@@ -1,11 +1,13 @@
 # The profile to set up the Nova controller (several services)
 class openstack::profile::nova::api {
+
   openstack::resources::controller { 'nova': }
   openstack::resources::database { 'nova': }
-  openstack::resources::firewall { 'Nova API': port => '8774', }
+
+  openstack::resources::firewall { 'Nova API':      port => '8774', }
   openstack::resources::firewall { 'Nova Metadata': port => '8775', }
-  openstack::resources::firewall { 'Nova EC2': port => '8773', }
-  openstack::resources::firewall { 'Nova S3': port => '3333', }
+  openstack::resources::firewall { 'Nova EC2':      port => '8773', }
+#  openstack::resources::firewall { 'Nova S3':       port => '3333', }
 
   class { '::nova::keystone::auth':
     password         => hiera('openstack::nova::password'),
