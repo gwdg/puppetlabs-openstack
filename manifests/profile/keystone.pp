ee9b6594c4fd3225ad27310ba480b47321c1790d
Piotr Kasprzak <piotr.kasprzak@gwdg.de>
2014-07-12 15:17:46 +0200

    LDAP setup for Keystone

--- manifests/profile/keystone.pp
+++ manifests/profile/keystone.pp
@@ -14,6 +14,21 @@ class openstack::profile::keystone {
     region           => hiera('openstack::region'),
   }
 
+  # Keystone LDAP setup
+  if ( hiera('openstack::keyston::ldap') == true ) {
+
+    class { 'keystone::ldap':
+
+      url            => "ldap://${hiera('openstack::ldap::address::management')}",
+      user           => 'cn=admin,dc=computecloud,dc=gwdg,dc=de',
+      password       => 'test',
+      suffix         => 'dc=computecloud,dc=gwdg,dc=de',
+      user_tree_dn   => 'ou=Users,dc=computecloud,dc=gwdg,dc=de',
+      tenant_tree_dn => 'ou=Groups,dc=computecloud,dc=gwdg,dc=de',
+      role_tree_dn   => 'ou=Roles,dc=computecloud,dc=gwdg,dc=de'
+    }
+  }
+
   $tenants = hiera('openstack::tenants')
   $users = hiera('openstack::users')
   create_resources('openstack::resources::tenant', $tenants)
