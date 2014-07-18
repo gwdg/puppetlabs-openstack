d9421f8c5c928c49f859773f716d30cc104c08cd
Piotr Kasprzak <piotr.kasprzak@gwdg.de>
2014-07-18 11:04:21 +0200

    Use controller node as database target

--- manifests/resources/database.pp
+++ manifests/resources/database.pp
@@ -1,8 +1,9 @@
 define openstack::resources::database () {
   class { "::${title}::db::mysql":
     user          => $title,
-    password      => hiera("openstack::mysql::service_password"),
+    password      => hiera('openstack::mysql::service_password'),
     dbname        => $title,
+    host          => hiera('openstack::controller::address::management'),
     allowed_hosts => hiera('openstack::mysql::allowed_hosts'),
     mysql_module  => '2.2',
     require       => Anchor['database-service'],
