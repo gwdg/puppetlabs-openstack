f68f95c2ddf74e9edaa7b0d3ded4b633fc951136
Piotr Kasprzak <piotr.kasprzak@gwdg.de>
2014-07-18 16:13:05 +0200

    Restructered nova api / compute roles (removed common/nova.pp, no qemu/libvirt stuff on api node)

--- manifests/profile/nova/compute.pp
+++ manifests/profile/nova/compute.pp
@@ -4,6 +4,9 @@ class openstack::profile::nova::compute {
   $management_network = hiera('openstack::network::management')
   $management_address = ip_for_network($management_network)
 
+  $storage_management_address = hiera('openstack::storage::address::management')
+  $controller_management_address = hiera('openstack::controller::address::management')
+
   # Additional configuration for nova
   
   # Metadata access with quantum: currently this is set in "nova::api", which is only included for controller nodes and multi_host = true compute nodes (i.e. with oldschool nova networking)
@@ -20,8 +23,35 @@ class openstack::profile::nova::compute {
   # Enable libvirt password injection
   nova_config { 'DEFAULT/libvirt_inject_password':              value => true }
 
-  class { '::compute::common::nova':
-    is_compute => true,
+  class { '::nova':
+    sql_connection     => $::openstack::resources::connectors::nova,
+    glance_api_servers => "http://${storage_management_address}:9292",
+    memcached_servers  => ["${controller_management_address}:11211"],
+    rabbit_hosts       => [$controller_management_address],
+    rabbit_userid      => hiera('openstack::rabbitmq::user'),
+    rabbit_password    => hiera('openstack::rabbitmq::password'),
+    debug              => hiera('openstack::debug'),
+    verbose            => hiera('openstack::verbose'),
+    mysql_module       => '2.2',
+  }
+
+  # TODO: it's important to set up the vnc properly
+  class { '::nova::compute':
+    enabled                       => true,
+    vnc_enabled                   => true,
+    vncserver_proxyclient_address => $management_address,
+    vncproxy_host                 => hiera('openstack::controller::address::vnc_proxy'),
+  }
+
+  class { '::nova::compute::neutron':
+#    libvirt_vif_driver => 'nova.virt.libvirt.vif.LibvirtHybridOVSBridgeDriver',
+  }
+
+  class { '::nova::network::neutron':
+    neutron_admin_password => hiera('openstack::neutron::password'),
+    neutron_region_name    => hiera('openstack::region'),
+    neutron_admin_auth_url => "http://${controller_management_address}:35357/v2.0",
+    neutron_url            => "http://${controller_management_address}:9696",
   }
 
   class { '::nova::compute::libvirt':
@@ -63,3 +93,4 @@ class openstack::profile::nova::compute {
 
 #  Package['libvirt'] -> File['/etc/libvirt/qemu.conf']
 }
+
