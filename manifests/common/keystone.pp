2c1230017bc782c818030ad0900a35ad2c9a5acc
Piotr Kasprzak <piotr.kasprzak@gwdg.de>
2014-07-20 07:23:43 +0200

    Moved keystone installation stuff from common/keystone.pp to the keystone profile. Also use new *_bind_host parameters and set admin_endpoint, so that all custom keystone providers use management ip to access keystone and not localhost.

--- manifests/common/keystone.pp
+++ manifests/common/keystone.pp
@@ -1,18 +1,24 @@
 class openstack::common::keystone {
-  if $::openstack::profile::base::is_controller {
-    $admin_bind_host = '0.0.0.0'
-  } else {
-    $admin_bind_host = hiera('openstack::controller::address::management')
-  }
+#  if $::openstack::profile::base::is_controller {
+#    $admin_bind_host = '0.0.0.0'
+#  } else {
+#    $admin_bind_host = hiera('openstack::controller::address::management')
+#  }
+
+  $bind_host = hiera('openstack::controller::address::management')
+  $admin_port = '35357'
 
   class { '::keystone':
-    admin_token     => hiera('openstack::keystone::admin_token'),
-    sql_connection  => $::openstack::resources::connectors::keystone,
-    verbose         => hiera('openstack::verbose'),
-    debug           => hiera('openstack::debug'),
-    enabled         => $::openstack::profile::base::is_controller,
-    admin_bind_host => $admin_bind_host,
-    mysql_module    => '2.2',
+    admin_token         => hiera('openstack::keystone::admin_token'),
+    sql_connection      => $::openstack::resources::connectors::keystone,
+    verbose             => hiera('openstack::verbose'),
+    debug               => hiera('openstack::debug'),
+    enabled             => $::openstack::profile::base::is_controller,
+    admin_bind_host     => $bind_host,
+    public_bind_host    => $bind_host,
+    admin_port          => $admin_port,
+    admin_endpoint      => "http://${bind_host}:${admin_port}/v2.0/",
+    mysql_module        => '2.2',
   }
 
   class { '::keystone::roles::admin':
--- manifests/profile/keystone.pp
+++ manifests/profile/keystone.pp
@@ -1,7 +1,7 @@
 # The profile to install the Keystone service
 class openstack::profile::keystone {
 
-  openstack::resources::controller { 'keystone': }
+#  openstack::resources::controller { 'keystone': }
 #  openstack::resources::database { 'keystone': }
 
   openstack::resources::firewall { 'Keystone API': 
@@ -10,7 +10,35 @@ class openstack::profile::keystone {
     port        => '5000', 
   }
 
-  include ::openstack::common::keystone
+#  include ::openstack::common::keystone
+
+#  if $::openstack::profile::base::is_controller {
+#    $admin_bind_host = '0.0.0.0'
+#  } else {
+#    $admin_bind_host = hiera('openstack::controller::address::management')
+#  }
+
+  $bind_host = hiera('openstack::controller::address::management')
+  $admin_port = '35357'
+
+  class { '::keystone':
+    admin_token         => hiera('openstack::keystone::admin_token'),
+    sql_connection      => $::openstack::resources::connectors::keystone,
+    verbose             => hiera('openstack::verbose'),
+    debug               => hiera('openstack::debug'),
+    enabled             => true,
+    admin_bind_host     => $bind_host,
+    public_bind_host    => $bind_host,
+    admin_port          => $admin_port,
+    admin_endpoint      => "http://${bind_host}:${admin_port}/v2.0/",
+    mysql_module        => '2.2',
+  }
+
+  class { '::keystone::roles::admin':
+    email        => hiera('openstack::keystone::admin_email'),
+    password     => hiera('openstack::keystone::admin_password'),
+    admin_tenant => 'admin',
+  }
 
   class { 'keystone::endpoint':
     public_address   => hiera('openstack::controller::address::api'),
