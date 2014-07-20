a13f18222c324894176887dada3de0dbe8ca9553
Piotr Kasprzak <piotr.kasprzak@gwdg.de>
2014-07-20 08:15:05 +0200

    Use uniform, utf8 based collate / charset parameters

--- manifests/resources/database.pp
+++ manifests/resources/database.pp
@@ -1,3 +1,4 @@
+# Wrapper for *::db::mysql stuff
 define openstack::resources::database () {
   class { "::${title}::db::mysql":
     user          => $title,
@@ -6,6 +7,8 @@ define openstack::resources::database () {
     host          => hiera('openstack::controller::address::management'),
     allowed_hosts => hiera('openstack::mysql::allowed_hosts'),
     mysql_module  => '2.2',
+    collate       => 'utf8_unicode_ci',
+    charset       => 'utf8',
     require       => Anchor['database-service'],
   }
 }
