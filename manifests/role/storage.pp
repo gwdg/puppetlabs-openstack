5e40fafad4b1a3b18c29b2d8af5d21e1797a8c59
Piotr Kasprzak <piotr.kasprzak@gwdg.de>
2014-07-21 17:46:02 +0200

    Generate auth file for storage node

--- manifests/role/storage.pp
+++ manifests/role/storage.pp
@@ -2,6 +2,7 @@ class openstack::role::storage inherits ::openstack::role {
   class { '::openstack::profile::firewall': }
   class { '::openstack::profile::glance::api': }
   class { '::openstack::profile::cinder::volume': }
+  class { '::openstack::profile::auth_file': }
 
   class { '::openstack::setup::cirros': }
 }
