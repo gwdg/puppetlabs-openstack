a99b0d80b1a5251d13c862b9baa7b0d2330f4347
Piotr Kasprzak <piotr.kasprzak@gwdg.de>
2014-07-21 16:54:07 +0200

    Generate auth file also on network node

--- manifests/role/network.pp
+++ manifests/role/network.pp
@@ -3,4 +3,5 @@ class openstack::role::network inherits ::openstack::role {
   class { '::openstack::profile::neutron::router': }    ->
   class { '::openstack::profile::neutron::server': }    ->
   class { '::openstack::setup::sharednetwork': }
+  class { '::openstack::profile::auth_file': }
 }
