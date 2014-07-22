9b758bca1cbdfd03e2e8a75cd4d57d78a3cec47a
Piotr Kasprzak <piotr.kasprzak@gwdg.de>
2014-07-22 14:48:15 +0200

    Do not provision network structures (fails due to keystone being on another host)

--- manifests/role/network.pp
+++ manifests/role/network.pp
@@ -2,6 +2,6 @@ class openstack::role::network inherits ::openstack::role {
   class { '::openstack::profile::firewall': }
   class { '::openstack::profile::neutron::router': }    ->
   class { '::openstack::profile::neutron::server': }    ->
-  class { '::openstack::setup::sharednetwork': }
+#  class { '::openstack::setup::sharednetwork': }
   class { '::openstack::profile::auth_file': }
 }
