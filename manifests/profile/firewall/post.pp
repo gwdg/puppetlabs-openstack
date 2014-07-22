e22c62dddf6b5c66d2ccf307a8a369ffbc4434a2
Piotr Kasprzak <piotr.kasprzak@gwdg.de>
2014-07-22 09:45:34 +0200

    Log rejected packets

--- manifests/profile/firewall/post.pp
+++ manifests/profile/firewall/post.pp
@@ -1,11 +1,23 @@
 # post-firewall rules to reject remaining traffic
 class openstack::profile::firewall::post {
+
   firewall { '8999 - Accept all management network traffic':
     proto  => 'all',
     state  => ['NEW'],
     action => 'accept',
     source => hiera('openstack::network::management'),
-  }  ->
+  } ->
+
+  # Log all rejected traffic
+  firewall { '9998 - Log rejected remaining traffic':
+    proto       => 'all',
+#    limit       => '2/min',
+    log_level   => 'warn',
+    log_prefix  => '[IPTABLES] dropped:',
+    jump        => 'LOG',
+  } ->
+
+  # Reject all remaining traffic
   firewall { '9999 - Reject remaining traffic':
     proto  => 'all',
     action => 'reject',
