dd58042f7f54dc841010b6d2d286f87d08b42fc7
Piotr Kasprzak <piotr.kasprzak@gwdg.de>
2014-07-24 10:17:43 +0200

    Allow overly network traffic

--- manifests/role/network.pp
+++ manifests/role/network.pp
@@ -4,4 +4,14 @@ class openstack::role::network inherits ::openstack::role {
   class { '::openstack::profile::neutron::server': }    ->
 #  class { '::openstack::setup::sharednetwork': }
   class { '::openstack::profile::auth_file': }
+
+
+  # Allow all tunneling traffic on data network
+  firewall { '8998 - Accept all data (overlay) network traffic':
+    proto       => 'all',
+    action      => 'accept',
+    source      => hiera('openstack::network::data'),
+    destination => ip_for_network(hiera('openstack::network::data')),
+    before      => [ Class['::firewall'] ],
+  }
 }
